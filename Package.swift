baaf7addce868cb709523379d290a79466d86345
Stephen Mitchell <scum@mac.com>
2021-02-15 15:02:38 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "SwiftyCam"
+    name: "SwiftyCam",
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
@@ -20,6 +20,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "SwiftyCam",
-            dependencies: []),
-        ]
+            dependencies: []
+        )
+    ]
 )
